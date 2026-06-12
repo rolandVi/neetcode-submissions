c19134d9cb100b2c2658e7085691873c36a63710
Roland Vízner <148648143+rolandVi@users.noreply.github.com>
2026-06-12 13:18:36 +0200

    Add: valid-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-1.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        self.sol = True
+        
+        def min_max_of_children(node: TreeNode) -> List[int]:
+            if not self.sol:
+                return [0, 0]
+            value = node.val
+            min_l, max_l, min_r, max_r = value, value, value, value
+            if node.left is not None:
+                min_l, max_l = min_max_of_children(node.left)
+                if max_l >= value:
+                    self.sol = False
+            
+            if node.right is not None:
+                min_r, max_r = min_max_of_children(node.right)
+                if min_r <= value:
+                    self.sol = False
+            
+            return [min_l, max_r]
+
+        min_max_of_children(root)
+        return self.sol
+        
+        
+
+
+
